92ec3a0164be45eecba9900bcddc898b7417b1c9
mckgar <kmgarner23@outlook.com>
2022-04-25 18:56:47 -0600

    Implemented minimax algorithm for ai player decision making

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,6 +9,13 @@ const gameBoard = (() => {
   let currentPlayer;
 
   const getCurrentPlayer = () => currentPlayer;
+  const getBoard = () => {
+    let temp = [];
+    for(let i = 0; i < board.length; i++) {
+      temp[i] = board[i];
+    }
+    return temp;
+  };
   
   const startGame = () => {
     currentPlayer = playerOne;
@@ -58,13 +65,13 @@ const gameBoard = (() => {
         if(currentPlayer.getPiece() == playerOne.getPiece()) {
           currentPlayer = playerTwo;
           if(playerTwoAI) {
-            playerTwo.makeMove();
+            playerTwo.makeMove(playerOne.getPiece());
           }
         } 
         else {
           currentPlayer = playerOne;
           if(playerOneAI) {
-            playerOne.makeMove();
+            playerOne.makeMove(playerTwo.getPiece());
           }
         }
       }
@@ -104,17 +111,7 @@ const gameBoard = (() => {
       displayController.displayWinner(currentPlayer);
     }
   };
-  const toString = () => {
-    let string = "";
-    for(let i = 0; i < board.length; i++) {
-      string += board[i];
-      if((i+1)%3==0) {
-        string += "\n"
-      }
-    }
-    return string;
-  };
-  return {startGame, createPlayers, makeMove, legalMove, getCurrentPlayer, toString};
+  return {startGame, createPlayers, makeMove, legalMove, getCurrentPlayer, getBoard};
 })();
 
 const displayController = (() => {
@@ -188,14 +185,110 @@ const player = (name, piece) => {
 const ai = (name, piece) => {
   const prototype = player(name, piece);
 
-  const makeMove = () => {
+  const makeMove = (opponent) => {
+    /* let move = __randomMove(); */
+    let move = __minimax(0, 3, gameBoard.getBoard(), opponent, true, prototype.getPiece())[0];
+    if(gameBoard.legalMove(move)) {
+      gameBoard.makeMove(displayController.getPosition(move));
+    }
+    else {
+      console.log("Illegal move at " + move);
+    }
+  };
+
+  const __randomMove = () => {
     while(true) {
       let move = Math.floor(Math.random() * 9);
       if(gameBoard.legalMove(move)) {
-        gameBoard.makeMove(displayController.getPosition(move));
-        break;
+        return move;
       }
     }
+  }
+
+  const __minimax = (depth, maxDepth, currentState, opponent, maximizer, currentPlayer) => {
+    if(__checkWinner(prototype.getPiece(), currentState)) {
+      return [-1, 1];
+    }
+    else if(__checkWinner(opponent, currentState)) {
+      return [-1, -1];
+    }
+    else if(__checkStalemate(currentState) || depth == maxDepth) {
+      return [-1, 0];
+    }
+
+    let legalMoves = [];
+    for(let i = 0; i < currentState.length; i++) {
+      if(currentState[i] == "_") {
+        legalMoves.push(i);
+      }
+    }
+
+    let scores = new Map();
+    for(let i of legalMoves) {
+      currentState = __testMove(currentState, currentPlayer, i);
+      if(currentPlayer == prototype.getPiece()) {
+        scores.set(i, __minimax(depth+1, maxDepth, currentState, opponent, false, opponent)[1]); 
+      }
+      else {
+        scores.set(i, __minimax(depth+1, maxDepth, currentState, opponent, true, prototype.getPiece())[1]);
+      }
+      currentState = __undoTestMove(currentState, i);
+    }
+
+    let bestScore;
+    let bestMove = -1;
+    if(maximizer) {
+      bestScore = -Infinity;
+      for(let pair of scores.entries()) {
+        if(pair[1] > bestScore) {
+          bestScore = pair[1];
+          bestMove = pair[0];
+        }
+      }
+    }
+    else {
+      bestScore = Infinity;
+      for(let pair of scores.entries()) {
+        if(pair[1] < bestScore) {
+          bestScore = pair[1];
+          bestMove = pair[0];
+        }
+      }
+    }
+
+    return [bestMove, bestScore];
+  };
+
+  const __checkWinner = (currentPlayer, board) => {
+    return (
+      (board[0] == currentPlayer && board[0] == board[1] && board[1] == board[2]) ||
+      (board[4] == currentPlayer && board[3] == board[4] && board[4] == board[5]) ||
+      (board[6] == currentPlayer && board[6] == board[7] && board[7] == board[8]) ||
+      (board[0] == currentPlayer && board[0] == board[3] && board[3] == board[6]) ||
+      (board[1] == currentPlayer && board[1] == board[4] && board[4] == board[7]) ||
+      (board[2] == currentPlayer && board[2] == board[5] && board[5] == board[8]) ||
+      (board[0] == currentPlayer && board[0] == board[4] && board[4] == board[8]) ||
+      (board[2] == currentPlayer && board[2] == board[4] && board[4] == board[6])
+    )
+  };
+
+  const __checkStalemate = (board) => {
+    for(let i=0; i<board.length; i++) {
+      if(board[i] == '_') {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const __testMove = (board, currentPlayer, position) => {
+    board[position] = currentPlayer;
+    return board;
+  };
+
+  const __undoTestMove = (board, position) => {
+    board[position] = "_";
+    return board;
   };
 
   return Object.assign({}, prototype, {makeMove});
